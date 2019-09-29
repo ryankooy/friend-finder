7a4e22a9dfb3c05f1e62648bd43d10455688b9ec
Ryan Kooy <rwkooy@gmail.com>
2019-09-29 08:35:35 -0400

    starting on survey.html

--- server.js
+++ server.js
@@ -10,5 +10,5 @@ require("./app/routing/apiRoutes")(app);
 require("./app/routing/htmlRoutes")(app);
 
 app.listen(PORT, function() {
-    console.log("App is listening on PORT " + PORT);
+    console.log(`App is listening on ${PORT}`);
 });
